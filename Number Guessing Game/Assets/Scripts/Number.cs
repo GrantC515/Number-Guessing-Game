5f7b82a22816a3df44b196d40b5ba27cffc9ad5e
GrantC515 <111007253+GrantC515@users.noreply.github.com>
2022-08-18 11:04:36 -0700

    Update

--- Number Guessing Game/Assets/Scripts/Number.cs
+++ Number Guessing Game/Assets/Scripts/Number.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 
 public class Number : MonoBehaviour
 {
-    int guess;
+    int guess = 50;
     int minValue = 1;
-    int maxValue = 10;
+    int maxValue = 100;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Welcome to our Number Guessing Game.");
-        Debug.Log("The number range is from " + minValue + " to " + maxValue );
+        Debug.Log("The number range is from " + minValue + " to " + maxValue);
+        Debug.Log("Guess a number between the given range.");
+        Debug.Log("Tell me if your number is higher or lower than: " + guess + "?");
+        Debug.Log("Push Up arrow = Higher, Push Down arrow = Lower, Push Enter = Correct");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            minValue = guess;
+            guess = (maxValue + minValue) / 2;
+            Debug.Log("Is it higher or lower than: " + guess);
+        }
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            maxValue = guess;
+            guess = (maxValue + minValue) / 2;
+            Debug.Log("is it higher or lower than: " + guess);
+        }
     }
 }
